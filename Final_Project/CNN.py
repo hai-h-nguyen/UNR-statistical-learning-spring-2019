f932d4770dfb10d88ee24560a611a62bc03a1cb5
HaiNguyen-LabComp <hainhbkhn@gmail.com>
2019-05-13 16:00:31 -0700

    Finish question 1

--- Final_Project/CNN.py
+++ Final_Project/CNN.py
@@ -9,6 +9,7 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.callbacks import ModelCheckpoint
 from keras.callbacks import TensorBoard
+from keras.layers.normalization import BatchNormalization
 
 # sklearn
 from sklearn import svm, datasets
@@ -18,6 +19,7 @@ from sklearn.utils.multiclass import unique_labels
 
 # Other stuff
 import os
+import sys
 from time import time
 import numpy as np
 import matplotlib.pyplot as plt
@@ -67,7 +69,7 @@ def plot_confusion_matrix(y_true, y_pred, classes,
 
 batch_size = 32
 num_classes = 10
-epochs = 150
+epochs = 1000
 data_augmentation = True
 save_dir = os.path.join(os.getcwd(), 'saved_models')
 model_name = 'keras_cifar10_trained_model.h5'
@@ -83,27 +85,35 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), padding='same',
+model.add(Conv2D(32, (5, 5), padding='same',
                  input_shape=x_train.shape[1:]))
 model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3)))
+model.add(Conv2D(64, (5, 5), padding = 'same'))
+model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
 
-model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Conv2D(128, (3, 3), padding='same'))
+model.add(BatchNormalization())
 model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
+model.add(Conv2D(128, (3, 3), padding = 'same'))
+model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
 
 model.add(Flatten())
 model.add(Dense(512))
+model.add(BatchNormalization())
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
+model.add(Dense(256))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
 model.add(Dense(num_classes))
+model.add(BatchNormalization())
 model.add(Activation('softmax'))
+
+model.summary()
+#sys.exit(0)
 
 # initiate RMSprop optimizer
 opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
